d093ecdc4ca7b40e1f2833f3f086152e0681dda9
Guga-Santos <guga.santos.guga@gmail.com>
2024-10-01 15:38:15 -0300

    Feat: Imports at Restaurante view

--- back/restaurantes/views.py
+++ back/restaurantes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from restaurantes.models import Restaurantes
+from restaurantes.api.serializers import RestaurantesSerializers
 
 # Create your views here.
+
+
