4d475e61971f4087b89a2ad618d698b4658dde01
Guga-Santos <guga.santos.guga@gmail.com>
2024-10-01 07:38:08 -0300

    Feat: create Campanha serializer

---
+++ back/campanha/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from campanha.models import Campanha
+
+class CampanhaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Campanha
+        fields = ['id_campanha', 'restaurante', 'titulo', 'descricao', 'data_inicio', 'data_fim', 'quantidade', 'tipo', 'disponivel']
