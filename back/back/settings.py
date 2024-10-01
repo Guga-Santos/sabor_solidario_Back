7efaa32bc914c4893d7dbec165d3faef304801c6
Guga-Santos <guga.santos.guga@gmail.com>
2024-10-01 17:20:03 -0300

    Fix: Restaurante Crud

--- back/back/settings.py
+++ back/back/settings.py
@@ -126,3 +126,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Adicione isso ao seu settings.py
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',  # Para retornar JSON
+        'rest_framework.renderers.BrowsableAPIRenderer',  # Para mostrar o formulário HTML
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ),
+}
+
--- back/restaurantes/views.py
+++ back/restaurantes/views.py
@@ -37,29 +37,37 @@ def Find_By_Id(request, pk):
     serializer = RestaurantesSerializers(restaurante, many=False)
     return Response(serializer.data)
    
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def Create(request):
-    serializer = RestaurantesSerializers(data=request.data)
+    if request.method == 'GET':
+        serializer = RestaurantesSerializers()
+        return Response(serializer.data) 
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'POST':
+        serializer = RestaurantesSerializers(data=request.data)
 
-@api_view(['PUT'])
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT'])
 def Update(request, pk):
-    try:
-        restaurante = Restaurantes.objects.get(id_restaurante=pk)
-    except Restaurantes.DoesNotExist:
-        return Response({'error': 'Restaurante não encontrado'}, status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = RestaurantesSerializers(instance= restaurante, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'message': 'Atualizado com Sucesso!'}, status=status.HTTP_200_OK)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        serializer = RestaurantesSerializers()
+        return Response(serializer.data) 
+    elif request.method == 'POST':
+        try:
+            restaurante = Restaurantes.objects.get(id_restaurante=pk)
+        except Restaurantes.DoesNotExist:
+            return Response({'error': 'Restaurante não encontrado'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = RestaurantesSerializers(instance= restaurante, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Atualizado com Sucesso!'}, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def Delete(request, pk):
