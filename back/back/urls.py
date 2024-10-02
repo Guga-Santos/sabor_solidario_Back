f67eddc22bda4b73ac658914bf6dbac7dea66e0d
Guga-Santos <guga.santos.guga@gmail.com>
2024-10-02 08:01:28 -0300

    Feat: create api/ route

--- back/back/urls.py
+++ back/back/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 
 from rest_framework import routers
 from transacao.api import viewsets as transacoes
+from campanha.api import viewsets as campanhas
+from restaurantes.api import viewsets as restaurantes
+from voluntarios.api import viewsets as voluntarios
 
 from restaurantes.api.routes import RestauranteRoutes
 from voluntarios.api.routes import VoluntariosRoutes
@@ -27,6 +30,9 @@ from transacao.api.routes import TransacoesRoutes
 
 route = routers.DefaultRouter()
 route.register('transacoes', transacoes.TransacaoViewSet, basename='Transacoes')
+route.register('campanhas', campanhas.CampanhaViewSet, basename='Campanhas')
+route.register('restaurantes', restaurantes.RestauranteViewSet, basename='Restaurantes')
+route.register('voluntarios', voluntarios.VoluntariosViewSet, basename='Voluntarios')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,5 +40,5 @@ urlpatterns = [
     path('voluntarios/', include(VoluntariosRoutes())),
     path('campanhas/', include(CampanhasRoutes())),
     path('transacoes/', include(TransacoesRoutes())),
-    path('', include(route.urls)),
+    path('api/', include(route.urls)),
 ]
